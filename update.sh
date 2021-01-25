4a04fbb457e8f4cfa5727ade6da1b761544f89eb
Osamu Aoki <osamu@debian.org>
2021-01-25 17:38:49 +0900

    Update update.sh
    Signed-off-by: Osamu Aoki <osamu@debian.org>

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 # vim:set ai si sts=2 sw=2 et:
+# start with -f to force page rebuild
 
 DATE=$(date -u --iso=sec)
 
@@ -7,9 +8,12 @@ echo -e "\033[0;32mList draft pages...\033[0m"
 hugo list drafts
 
 if git diff --exit-code >/dev/null ; then
-  echo -e "\033[0;32mSource not changed from the last commit\033[0m"
+  echo -e "\033[0;31mSource not changed from the last commit\033[0m"
+  if [ "$1" != "-f" ]; then
+    exit
+  fi
 else
-  echo "\033[0;32mSource changed from the last commit\033[0m"
+  echo -e "\033[0;32mSource changed from the last commit\033[0m"
   git commit -a -m "source updated: $DATE"
 fi
 
@@ -26,7 +30,10 @@ echo -e "\033[0;32mUploading HTML pages...\033[0m"
 cd public
 # Add changes to git.
 git add -A *
-git commit -m "HTML rebuilt: $DATE"
+if git commit -m "HTML rebuilt: $DATE"; then
+  echo -e "\031[0;34mNothing to commit .... Finished\033[0m"
+  exit
+fi
 git push origin master
 # Come Back up to the Project Root
 cd ..
@@ -34,3 +41,5 @@ cd ..
 # record submodule updates
 git commit -a -m "submodule updated: $DATE"
 git push origin master
+echo -e "\033[0;34mFinished\033[0m"
+
