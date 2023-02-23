12bbb5517dc45b86b15472605f463e0e80e29bdd
Osamu Aoki <osamu@debian.org>
2023-02-23 13:43:29 +0900

    source updated: 2023-02-23T04:43:29+00:00

--- repository.sh
+++ repository.sh
@@ -2,15 +2,41 @@
 # vim:set ai si sts=2 sw=2 et:
 # start with -f to force page rebuild
 
-# can be started from sub-directory
+
+# Directory name for repository found in the parent directory
+REPODIR0="packages"
+
+# can be started from sub-directory in this source
 cd "$(dirname "$(which "$0")")"
-
-rm -rf static/debian/db
-rm -rf static/debian/pool
-rm -rf static/debian/dists
-
 BASEDIR="$(pwd)/static/debian"
+REPODIR="$(pwd)/../$REPODIR0"
 
-for f in ../packages/*.changes; do
+cd "$REPODIR"
+
+rm -f ./*.deb
+rm -f ./*.changes
+rm -f ./*.dsc
+rm -f ./*.buildinfo
+rm -f ./*.build
+rm -f ./*.xz
+#
+if [ "$1" = "-c" ]; then
+  exit 0
+fi
+rm -rf "$BASEDIR/static/debian/db"
+rm -rf "$BASEDIR/static/debian/pool"
+rm -rf "$BASEDIR/static/debian/dists"
+#
+for d in * ; do 
+  if [ -d "$d" ]; then
+    cd "$d"
+    git zap
+    debuild # -us -uc
+    cd ..
+  fi
+done
+#
+for f in *.changes; do
+  debsign "$f"
   reprepro --ignore=wrongdistribution -b "$BASEDIR" include sid "$f"
 done
